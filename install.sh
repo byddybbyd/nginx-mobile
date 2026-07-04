0318802e1a2b5477cc061a6ee98aeaf5997c8738
byddybbyd <178136455+byddybbyd@users.noreply.github.com>
2026-07-04 19:09:25 +0800

    rm install

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd ~/.termux
-mv termux.properties termux.properties.bak
-curl -LO https://raw.githubusercontent.com/byddybbyd/new/refs/heads/main/termux.properties
-curl -LO https://raw.githubusercontent.com/byddybbyd/new/refs/heads/main/font.ttf
-termux-reload-settings
-echo "Done"
